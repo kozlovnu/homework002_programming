267a516031319b2b7bc51a603e6fe380afc5b543
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-13 22:16:52 +0300

    Add task010

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8 
+//918 -> 1
+Console.WriteLine("Enter your number between 100 and 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100 && number < 1000)
+{
+    int two = number / 10;
+    double result = two % 10;
+    Console.WriteLine("The second number is " + result);
+}
+else 
+{
+    Console.WriteLine("Your number is out of range.");
+}    
