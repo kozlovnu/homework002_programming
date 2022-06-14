0af3de424244b520afe2c7f0543a36758ec4eba4
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-14 21:20:08 +0300

    Add task013

---
+++ task013/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+double result = number % 10;
+Console.WriteLine(result);
+}
+
+else 
+{
+    Console.WriteLine("Your number is out of range");
+}
